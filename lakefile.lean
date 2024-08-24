38939078a9bfdecaba71c36f29d5538a5de01b92
Lean 4 VS Code Extension <>
2024-08-24 12:02:18 -0300

    Initial commit

---
+++ lakefile.lean
@@ -0,0 +1,15 @@
+import Lake
+open Lake DSL
+
+package "lean4-polyhedral-combinatorics" where
+  -- Settings applied to both builds and interactive editing
+  leanOptions := #[
+    ⟨`pp.unicode.fun, true⟩ -- pretty-prints `fun a ↦ b`
+  ]
+  -- add any additional package configuration options here
+
+require "leanprover-community" / "mathlib"
+
+@[default_target]
+lean_lib «Lean4PolyhedralCombinatorics» where
+  -- add any library configuration options here
